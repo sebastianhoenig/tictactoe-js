c3c2357fed963c8f1f894417aa13cb8a889c1f98
Sebastian Hönig <hoenigsebastian@gmail.com>
2021-12-23 22:47:48 +0100

    player vs player version finalized

--- script.js
+++ script.js
@@ -1,44 +1,188 @@
-let logicalBoard = [
-  ["O", "O", "O"],
-  ["O", "X", "X"],
-  ["X", "X", "X"],
-];
-const container = document.getElementById("container");
-let cells = document.getElementsByClassName("cell");
-
 const Player = (name, symbol) => {
-  const makeMove = (symbol) => console.log(`Makes move with ${symbol}`);
-  return { makeMove };
+  return { name, symbol };
 };
 
 const gameBoard = (() => {
-  for (let i = 0; i < 3; i++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("row");
-    if (i < 2) {
-      rowDiv.classList.add("row-line");
-    }
-    for (let j = 0; j < 3; j++) {
-      let cellDiv = document.createElement("div");
-      cellDiv.classList.add("cell");
-      cellDiv.setAttribute("data-row", i);
-      cellDiv.setAttribute("data-col", j);
-      if (j < 2) {
-        cellDiv.classList.add("cell-line");
+  let logicalBoard = ["", "", "", "", "", "", "", "", ""];
+  const container = document.getElementById("container");
+  let someoneHasWon = false;
+  let noNamesEntered = true;
+
+  container.addEventListener("click", (e) => {
+    if (someoneHasWon || noNamesEntered) {
+      return;
+    } else {
+      const player = game.getCurrentPlayer();
+      let cell = getTarget(e);
+      let cellId = cell.getAttribute("data-cell");
+      console.log(checkIfFree(cellId));
+      if (checkIfFree(cellId)) {
+        logicalBoard[cellId] = player.symbol;
       }
-      rowDiv.appendChild(cellDiv);
+      updateGameBoard();
+      game.checkWinner();
+      game.setNextPlayer();
     }
-    container.appendChild(rowDiv);
+  });
+
+  function getTarget(e) {
+    let target = e.target;
+    return target;
   }
+
+  let cells = [];
+
+  const setNoNamesEntered = (start) => {
+    noNamesEntered = start;
+  };
+
+  const createGameBoard = () => {
+    for (let i = 0; i < 3; i++) {
+      let rowDiv = document.createElement("div");
+      rowDiv.classList.add("row");
+      if (i < 2) {
+        rowDiv.classList.add("row-line");
+      }
+      for (let j = 0; j < 3; j++) {
+        let cellDiv = document.createElement("div");
+        cellDiv.classList.add("cell");
+        cellDiv.setAttribute("data-cell", i * 3 + j);
+        if (j < 2) {
+          cellDiv.classList.add("cell-line");
+        }
+        rowDiv.appendChild(cellDiv);
+        cells.push(cellDiv);
+      }
+      container.appendChild(rowDiv);
+    }
+  };
+
+  const updateGameBoard = () => {
+    for (i in cells) {
+      let currentCell = cells[i];
+      let cellId = currentCell.getAttribute("data-cell");
+      let positionText = logicalBoard[cellId];
+      currentCell.textContent = positionText;
+    }
+  };
+
+  const resetGameBoard = () => {
+    for (let i = 0; i < logicalBoard.length; i++) {
+      logicalBoard[i] = "";
+    }
+    for (cell in cells) {
+      cells[cell].classList.remove("winner");
+    }
+    updateGameBoard();
+    someoneHasWon = false;
+  };
+
+  const updateWinnerCells = (winCells) => {
+    console.log(winCells);
+    for (cell in cells) {
+      console.log(cells[cell].getAttribute("data-cell"));
+      if (
+        cells[cell].getAttribute("data-cell") == winCells[0] ||
+        cells[cell].getAttribute("data-cell") == winCells[1] ||
+        cells[cell].getAttribute("data-cell") == winCells[2]
+      ) {
+        cells[cell].classList.add("winner");
+      }
+    }
+    someoneHasWon = true;
+  };
+
+  const getCells = () => {
+    return cells;
+  };
+
+  const checkIfFree = (cellId) => {
+    let pos = logicalBoard[cellId];
+    if (pos === "") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return {
+    updateGameBoard,
+    createGameBoard,
+    resetGameBoard,
+    logicalBoard,
+    updateWinnerCells,
+    someoneHasWon,
+    getCells,
+    setNoNamesEntered,
+  };
 })();
 
-function renderBoard() {
-  for (cell in cells) {
-    console.log(cells[cell]);
-    let currentCell = cells[cell];
-    let rowId = currentCell.getAttribute("data-row");
-    let colId = currentCell.getAttribute("data-col");
-    let positionText = logicalBoard[rowId][colId];
-    currentCell.textContent = positionText;
-  }
-}
+const game = (() => {
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  let player1;
+  let player2;
+  let currentPlayer;
+  const resetButton = document.getElementById("reset");
+  const startButton = document.getElementById("start");
+  let gameText = document.getElementsByClassName("gametext")[0];
+  gameText.textContent = "Enter your names and click on Start";
+
+  resetButton.addEventListener("click", () => {
+    gameBoard.resetGameBoard();
+    gameBoard.setNoNamesEntered(true);
+    gameText.textContent = "Click on Start to play again!";
+  });
+
+  startButton.addEventListener("click", () => {
+    let playerOneForm = document.getElementById("player1");
+    let playerTwoForm = document.getElementById("player2");
+    if (playerOneForm.value === "") {
+      player1 = Player("Player 1", "X");
+    } else {
+      player1 = Player(playerOneForm.value, "X");
+    }
+    if (playerOneForm.value === "") {
+      player2 = Player("Player 2", "O");
+    } else {
+      player2 = Player(playerTwoForm.value, "O");
+    }
+    currentPlayer = player1;
+    gameBoard.setNoNamesEntered(false);
+  });
+
+  const getCurrentPlayer = () => {
+    return currentPlayer;
+  };
+
+  const setNextPlayer = () => {
+    currentPlayer === player1
+      ? (currentPlayer = player2)
+      : (currentPlayer = player1);
+  };
+
+  const checkWinner = () => {
+    for (i in winConditions) {
+      if (
+        gameBoard.logicalBoard[winConditions[i][0]] === currentPlayer.symbol &&
+        gameBoard.logicalBoard[winConditions[i][1]] === currentPlayer.symbol &&
+        gameBoard.logicalBoard[winConditions[i][2]] === currentPlayer.symbol
+      ) {
+        gameBoard.updateWinnerCells(winConditions[i]);
+        gameText.textContent = `Congratulations, ${currentPlayer.name} has won!`;
+      }
+    }
+  };
+
+  gameBoard.createGameBoard();
+
+  return { getCurrentPlayer, setNextPlayer, checkWinner };
+})();
